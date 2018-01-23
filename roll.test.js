a59a3e2448de78e8971f063a32d90552a7e1feb7
lakhara <siddharth.lakhara@gmail.com>
2018-01-23 12:06:24 +0530

    added describe

--- roll.test.js
+++ roll.test.js
@@ -1,37 +1,45 @@
 const roll = require('./roll');
 
-test('Base Case: ', () =>
-	expect(roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
-		toBe(90));
+describe('Basic cases: ', () => {
+	test('Base Case: ', () =>
+		expect(roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
+			toBe(90));
 
-test('Test for spare: ', () =>
-	expect(roll([4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
-		toBe(94));
+	test('Test for spare: ', () =>
+		expect(roll([4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
+			toBe(94));
 
-test('Test for strike: ', () =>
-	expect(roll([10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
-		toBe(100));
+	test('Test for strike: ', () =>
+		expect(roll([10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
+			toBe(100));
+});
 
-test('Test for spare at the end: ', () =>
-	expect(roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 4, 6, 2])).
-		toBe(93));
+describe('testing at end: ', () => {
+	test('Test for spare at the end: ', () =>
+		expect(roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 4, 6, 2])).
+			toBe(93));
 
-test('Test for strike at the end: ', () =>
-	expect(roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10, 6, 6])).
-		toBe(103));
+	test('Test for strike at the end: ', () =>
+		expect(roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10, 6, 6])).
+			toBe(103));
+});
 
-test('Test for multiple spare: ', () =>
-	expect(roll([3, 6, 4, 6, 4, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
-		toBe(104));
+//
 
-test('Test for multiple spare and strike at end: ', () =>
-	expect(roll([3, 6, 4, 6, 4, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10,6,6])).
-		toBe(117));
+describe('Multiple wins: ', () => {
+	test('Test for multiple spare: ', () =>
+		expect(roll([3, 6, 4, 6, 4, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
+			toBe(104));
 
-test('Test for spare and multiple strikes: ', () =>
-	expect(roll([4, 6, 10, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
-		toBe(125));
+	test('Test for multiple spare and strike at end: ', () =>
+		expect(roll([3, 6, 4, 6, 4, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10,6,6])).
+			toBe(117));
 
-test('Test for multiple spares & strikes: ', () =>
-	expect(roll([4, 6, 10, 10, 3, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
-		toBe(129));
+	test('Test for spare and multiple strikes: ', () =>
+		expect(roll([4, 6, 10, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
+			toBe(125));
+
+	test('Test for multiple spares & strikes: ', () =>
+		expect(roll([4, 6, 10, 10, 3, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
+			toBe(129));
+});
