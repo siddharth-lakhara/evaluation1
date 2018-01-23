0d31318473cb280499a57b3de3850888d470c647
lakhara <siddharth.lakhara@gmail.com>
2018-01-23 12:22:29 +0530

    Input validation added

--- roll.js
+++ roll.js
@@ -1,20 +1,47 @@
 function roll(pins){
 	let retArray = [];
 	let loop = 0;
-	for (let i=0; i<pins.length && loop<10; i = i+2, loop++){
-		if (pins[i] + pins[i+1] > 10){  // strike
-			retArray.push(10 + pins[i+1] + pins[i+2]);
-			i -= 1; // so that one frame is not missed
-		}
-		else if (pins[i] + pins[i+1] === 10){ // spare
-			retArray.push(10 + pins[i+2]);
-		}
-		else {
-			retArray.push(pins[i] + pins[i+1]);
+	if (validate(pins)){ // validate input
+		for (let i=0; i<pins.length && loop<10; i = i+2, loop++){
+			if (pins[i] + pins[i+1] > 10){  // strike
+				retArray.push(10 + pins[i+1] + pins[i+2]);
+				i -= 1; // so that one frame is not missed
+			}
+			else if (pins[i] + pins[i+1] === 10){ // spare
+				retArray.push(10 + pins[i+2]);
+			}
+			else {
+				retArray.push(pins[i] + pins[i+1]);
+			}
 		}
 	}
 	let totalScore = retArray.reduce((prev, curr)=>prev+curr,0);
 	return totalScore;
+}
+
+function validate(input){
+	if (input.length % 2 === 0){
+		return true;
+	}
+	let v1 = false; // validation 1 for number of strikes
+	let v2 = false; // validation 2 for spare at the end
+
+	{
+		let arr10 = input.reduce((prev, curr)=> {
+			if (curr === 10)
+				prev++;
+		}, 0);
+		if (arr10 % 2 != 0)
+			v1 = true;
+	}
+
+	{
+		let len = input.length;
+		if (input[len-1] + input[len-2] === 10) // spare at end is present
+			v2 = true;
+	}
+
+	return v1 || v2;
 }
 
 module.exports = roll;
